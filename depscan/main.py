8c980e042d90ead8e6e2d44094dc5aa809749aee
Vitali Andrushkevich <vitalis03@mail.ru>
2022-07-24 01:59:47 +0300

    First tries with md generator

--- depscan/main.py
+++ depscan/main.py
@@ -3,6 +3,8 @@ import os
 import json
 import re
 import os
+import requests
+from mdutils.mdutils import MdUtils
 
 
 def parse_dependancies(path):
@@ -23,7 +25,64 @@ def parse_dependancies(path):
     return data
 
 
+def check_package(package_name, version):
+    url = 'https://api.osv.dev/v1/query'
+    message = {
+        "version": version,
+        "package": {
+            "name": package_name,
+            "ecosystem": "PyPI"
+        }
+    }
+
+    return json.loads(requests.post(url, json=message).text)
+
+
+def markdown_create(file_name, title, json_parse):
+    mdFile = MdUtils(file_name=file_name,
+                     title=title)
+
+    vulnarabilities_list = []
+
+    for i in range(len(json_parse["vulns"])):
+        vulnarabilities_list.append({
+            "name": json_parse["vulns"][i]["affected"][0]["package"]["name"],
+            "summary": json_parse.get("vulns")[i]["summary"],
+            "CVE": json_parse.get("vulns")[i].get("aliases", "None"),
+            "affected_versions": json_parse.get("vulns")[i]["affected"][0]["versions"],
+            "fixed_version": json_parse.get("vulns")[i]["affected"][0]["ranges"][0]["events"][1]["fixed"],
+            "link": json_parse.get("vulns")[i]["affected"][0]["database_specific"]["source"]
+        })
+
+    rows = len(vulnarabilities_list)+1
+
+    list_of_keys = list(vulnarabilities_list[0].keys())
+    coloumns = len(list_of_keys)
+
+    formatted_list = []
+    formatted_list += list_of_keys
+
+    values_str_list = []
+
+    for item in vulnarabilities_list:
+        for value in list(item.values()):
+            values_str_list.append(str(value))
+
+    mdFile.new_line()
+    mdFile.new_table(columns=coloumns, rows=rows, text=formatted_list + values_str_list,
+                     text_align='center')
+
+    mdFile.create_md_file()
+
+
 if __name__ == '__main__':
-    path = os.getcwd()
-    obj = parse_dependancies(path)
-    print(json.dumps(obj, indent=2))
+    # path = os.getcwd()
+    # obj = parse_dependancies(path)
+    # print(json.dumps(obj, indent=2))
+
+    # print(check_package("treq", "17.3.1"))
+    markdown_create("treq.md", "Vulnarability for treq",
+                    check_package("treq", "17.3.1"))
+
+    markdown_create("treq2.md", "Vulnarability for treq",
+                    check_package("Pillow", "4.2.1"))
